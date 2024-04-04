4c688f2309ac4fb08c845b79b2727f78c57e7494
sharma171 <68746747+sharma171@users.noreply.github.com>
2024-04-04 14:58:29 +0530

    removed download and view

--- components/project/project.jsx
+++ components/project/project.jsx
@@ -26,34 +26,37 @@ const Project = () => {
                              <li key={i}>
                              <div className="card-inner">
                                 {/* <img src={el.image}/> */}
-                                 <Image src={el.image} width={400} height={400} className="thumb"/>
-                                 <div className="info">
+                                <Link href={el.project_link} className="text-link">
+                                    <Image src={el.image} width={400} height={400} className="thumb"/>
+                                    <div className="info">
 
-                                     <div className="tech-info">
-                                         {el.project_technologie && el.project_technologie.map((ab) =>(
-                                            <span>{ab.technology}</span>
-                                         ))}
-                                         
-                                     </div>
-                                     <Link href={el.project_link} className="text-link">
-                                         <h3 className="heading">
-                                            {el.name}
-                                         </h3>
-                                     </Link>
-                                     <p className="cardpara">
-                                        {el.project_description}
-                                     </p>
-                                     <div className="card-button">
-                                         <a href="" className="bot-button">
-                                             <Image src='./images/Download-icon.svg' width={20} height={20} alt='icon'/>
-                                             Download
-                                         </a>
-                                         <a href="" className="bot-button">
-                                             <Image src='./images/view-icon.svg' width={20} height={20} alt='icon'/>
-                                             View
-                                         </a>
-                                     </div>
-                                 </div>
+                                        <div className="tech-info">
+                                            {el.project_technologie && el.project_technologie.map((ab) =>(
+                                                <span>{ab.technology}</span>
+                                            ))}
+                                            
+                                        </div>
+                                        
+                                            <h3 className="heading">
+                                                {el.name}
+                                            </h3>
+                                        
+                                        <p className="cardpara">
+                                            {el.project_description}
+                                        </p>
+    {/*                                      
+                                        <div className="card-button">
+                                            <a href="" className="bot-button">
+                                                <Image src='./images/Download-icon.svg' width={20} height={20} alt='icon'/>
+                                                Download
+                                            </a>
+                                            <a href="" className="bot-button">
+                                                <Image src='./images/view-icon.svg' width={20} height={20} alt='icon'/>
+                                                View
+                                            </a>
+                                        </div> */}
+                                    </div>
+                                 </Link>
                              </div>
                          </li>
                            ))}                    
