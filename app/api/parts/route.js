eb0b0d0c1c511feda24f5e05d54bdfbd788234d9
lavranjansingh <niddusingh4@gmail.com>
2024-04-08 18:10:56 +0530

    dds

--- app/api/parts/route.js
+++ app/api/parts/route.js
@@ -89,7 +89,6 @@ export async  function POST(req){
 
 export async function PUT(req) {
   const {arrayOfTech,arrayOfCategory} = await req.json()
-  console.log(arrayOfCategory);
   // const redisdata = await client.get(`project${[...arrayOfTech,"Both",...arrayOfTech]}`);
   // if(!redisdata){
     let technologyFilter = ``
@@ -107,7 +106,6 @@ export async function PUT(req) {
     }
 
   const already =  `Select list.project_link, language.language,list.name,list.project_module, list.meta_tags, list.meta_keywords, list.meta_description, list.meta_title, list.project_technologie,  list.project_description, list.id from jtcindia_projects.project_lists as list Left Join jtcindia_projects.project_languages as language On language.id = list.project_language  WHERE deleted_by = '0'  ${technologyFilter} ${categoryFilter}`
-console.log(already);
   const data = await executeQuery(already)
 
   if(data.length == 0)  return NextResponse.json({data }, { success : false}, {status : 200})
--- app/newsection/page.jsx
+++ app/newsection/page.jsx
@@ -8,6 +8,8 @@ import Image from 'next/image'
 const Page = () => {
     const [filterActive, setfilterActive] = useState(true);
     const [domainActive, setdomainActive] = useState(true);
+    const [selectedTech, setSelectedITech] = useState([])
+    const [selectedCat, setSelectedCat] = useState([])
 
     const [project, setProjects] = useState([])
 
@@ -32,43 +34,96 @@ const Page = () => {
         const {data} =  await projectList(id)
    return data &&  setTech(...data) 
     }
-    let arrayOfTech = []
-    let arrayOfCategory = []
+    let arrayOfTech = [];
+    let arrayOfCategory = [];
+    
     const getSeletedTechnology = async (value) => {
         const index = arrayOfTech.indexOf(value);
-        if (index >= 0) 
-           return arrayOfTech.splice(1, index);
-         else 
-          return  arrayOfTech.push(value);
+        const updatedTechArray = [...arrayOfTech]; // Create a copy of arrayOfTech
         
-     
-      l
+        if (index >= 0) 
+           { updatedTechArray.splice(index, 1);
+            const { data } = await allFiltewr(updatedTechArray, arrayOfCategory);
+            
+           return data && setProjects(data);
+        }
+        else 
+           { updatedTechArray.push(value);
+        
+        const { data } = await allFiltewr(updatedTechArray, arrayOfCategory);
+      return  data && setProjects(data);
+    }
     }
     
     const getSeletedCategory = async (value) => {
         const index = arrayOfCategory.indexOf(value);
+        const updatedCategoryArray = [...arrayOfCategory]; // Create a copy of arrayOfCategory
         if (index >= 0) 
-            arrayOfCategory.splice(1, index);
-       else 
-            arrayOfCategory.push(value);
-    
-   console.log(arrayOfCategory);
+          {  updatedCategoryArray.splice(index, 1);
+            
+        const { data } = await allFiltewr(arrayOfTech, updatedCategoryArray);
+       return data && setProjects(data);
     }
+        else 
+        {    updatedCategoryArray.push(value);
+            
+        const { data } = await allFiltewr(arrayOfTech, updatedCategoryArray);
+       return data && setProjects(data);
+        }
+    }
+    
 
-    const dataFilter = async() => {
-        const { data } = await allFiltewr(arrayOfTech, arrayOfCategory);
-        getLists(0)
+    
+	function checkboxTechnology(e){
+		let isSelected = e.target.checked;
+    
+		let value = parseInt(e.target.value);
+		if( isSelected ){
+			setSelectedITech( [...selectedTech, value ] )
+
+		}else{
+			setSelectedITech((prevData)=>{
+				return prevData.filter((id)=>{
+					return id!==value
+				})
+			})
+		}
+
+	}
+    
+	function checkboxCategory(e){
+      
+		let isSelected = e.target.checked;
+		let value = e.target.value
+		if( isSelected ){
+			setSelectedCat( [...selectedCat, value ] )
+        }else{
+			setSelectedCat((prevData)=>{
+				return prevData.filter((id)=>{
+					return id!==value
+				})
+			})
+		}
+      
+	}
+
+
+    const filterData = async () => {
+        const {data}  = await allFiltewr(selectedTech, selectedCat);
         return data && setProjects(data);
     }
 
     const projects = async() => {
       
         const {data} =  await allProject()
-        getSeletedCategory()
-        getSeletedTechnology()
+       
         return data &&  setProjects(data)  
     }
 
+    
+useEffect(() => {
+    filterData()
+},[selectedTech, selectedCat])
    
     useEffect(() =>{
         allData()
@@ -138,7 +193,9 @@ const Page = () => {
                             </div>
                             <ul className={`filter-techlist ${filterActive ? 'active' : ''}`}>
                                 {tech && tech.technology && tech.technology.map((el) =>(
-                                    <li><label htmlhtmlFor="checklist2" >{el.technology}</label><input type="checkbox"   onClick={() => getSeletedTechnology(el.id)}/></li>
+//                         <input type="checkbox" checked={ selectedTech.includes( item.id ) } value={item.id} onChange={checkboxHandler}    />
+
+                                    <li><label htmlhtmlFor="checklist2" >{el.technology}</label><input type="checkbox"  checked={ selectedTech.includes( el.id ) } value={el.id} onChange={checkboxTechnology}     /></li>
 
                                 ))}
                                        </ul>
@@ -153,11 +210,11 @@ const Page = () => {
                             <ul className={`filter-techlist ${domainActive ? 'active' : ''}`}>
                                 {tech && tech.project && tech.project.map((el) =>(
 
-                                <li><label htmlhtmlFor="checklist3">{el.category}</label><input type="checkbox"  id={el.id} value={el.category} onClick={() => getSeletedCategory(el.category)}/></li>
+                                <li><label htmlhtmlFor="checklist3">{el.category}</label><input type="checkbox"  checked={ selectedCat.includes( el.category ) }  value={el.category} onChange={checkboxCategory}  /></li>
                                 ))}
                                 </ul>
                         </div>
-                        <div className="done-button" onClick={() =>  dataFilter()} style={{cursor : 'pointer'}}>
+                        <div className="done-button"  style={{cursor : 'pointer'}}>
                             Done
                         </div>
                     </div>
@@ -176,3 +233,170 @@ export default Page;
 
 
   
+
+
+// import { useState } from "react";
+
+// const List = () =>{
+//     const [data, setData] = useState(
+//         [
+//             {
+//               "userId": 1,
+//               "id": 1,
+//               "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//               "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 2,
+//               "title": "qui est esse",
+//               "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 3,
+//               "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//               "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 4,
+//               "title": "eum et est occaecati",
+//               "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 5,
+//               "title": "nesciunt quas odio",
+//               "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 6,
+//               "title": "dolorem eum magni eos aperiam quia",
+//               "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 7,
+//               "title": "magnam facilis autem",
+//               "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 8,
+//               "title": "dolorem dolore est ipsam",
+//               "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 9,
+//               "title": "nesciunt iure omnis dolorem tempora et accusantium",
+//               "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+//             },
+//             {
+//               "userId": 1,
+//               "id": 10,
+//               "title": "optio molestias id quia eum",
+//               "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+//             },
+//             {
+//               "userId": 2,
+//               "id": 11,
+//               "title": "et ea vero quia laudantium autem",
+//               "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+//             },
+//             {
+//               "userId": 2,
+//               "id": 12,
+//               "title": "in quibusdam tempore odit est dolorem",
+//               "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+//             },
+//             {
+//               "userId": 2,
+//               "id": 13,
+//               "title": "dolorum ut in voluptas mollitia et saepe quo animi",
+//               "body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+//             },
+//             {
+//               "userId": 2,
+//               "id": 14,
+//               "title": "voluptatem eligendi optio",
+//               "body": "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
+//             },
+//             {
+//               "userId": 2,
+//               "id": 15,
+//               "title": "eveniet quod temporibus",
+//               "body": "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
+//             }
+//         ]
+//     )
+
+// 	const [selectedTech, setSelectedITech] = useState([])
+	
+
+// 	function checkboxHandler(e){
+// 		let isSelected = e.target.checked;
+// 		let value = parseInt(e.target.value);
+
+// 		if( isSelected ){
+// 			setSelectedITech( [...selectedTech, value ] )
+// 		}else{
+// 			setSelectedITech((prevData)=>{
+// 				return prevData.filter((id)=>{
+// 					return id!==value
+// 				})
+// 			})
+// 		}
+// 	}
+
+
+// 	function checkAllHandler(){
+		
+
+// 		if( data.length === selectedTech.length ){
+// 			setSelectedITech( [] )
+// 		}else{
+// 			const postIds = data.map((item)=>{
+// 				return item.id
+// 			})
+
+// 			setSelectedITech( postIds )
+// 		}
+// 	}
+
+
+	
+
+//     return(
+//         <>
+//         <div className="container">
+//             <div className="left">
+//                 {data.map((item,index)=><div className="card" key={index}>
+//                     <label>
+						
+//                         <input type="checkbox" checked={ selectedTech.includes( item.id ) } value={item.id} onChange={checkboxHandler}    />
+//                     </label>
+//                     <h1>Id: {item.id}</h1>
+//                     <h2>{item.title}</h2>
+//                     <p>{item.body}</p>
+//                 </div>)}
+//             </div>
+//             <div className="right">
+//                 <div className="results">
+//                     <div>
+//                         <button type="button" onClick={checkAllHandler} >{ data.length === selectedTech.length ? 'Uncheck All' : 'Check all' }</button>
+//                     </div>
+
+//                     <h3>Result will print here: {selectedTech.toString()} </h3>
+
+//                 </div>
+//             </div>
+//             </div>
+//         </>
+        
+//     )
+// }
+
+// export default List;
